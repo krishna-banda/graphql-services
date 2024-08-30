ab37bc55a68d856e34e69799b725a5687b4904f8
Krishna Banda <krishna.banda87@gmail.com>
2024-08-29 21:39:40 -0400

    Sample Implemantation for GraphQL

---
+++ graphql-schema-first/src/main/java/com/krishna/banda/delegate/service/EmployeeDataService.java
@@ -0,0 +1,27 @@
+package com.krishna.banda.delegate.service;
+
+import com.krishna.banda.EmployeeAddress;
+import com.krishna.banda.EmployeeData;
+import com.krishna.banda.util.DataFetchersDelegateEmployeeData;
+import graphql.schema.DataFetchingEnvironment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class EmployeeDataService implements DataFetchersDelegateEmployeeData {
+    @Override
+    public EmployeeAddress address(DataFetchingEnvironment dataFetchingEnvironment, EmployeeData origin) {
+        //Find Address based on Employee Details
+        log.info("This is Entry Point for Employee Address");
+        //Observe the same Execution Id
+        log.info("Execution Id for the given request to understand the relation " + dataFetchingEnvironment.getExecutionId().toString());
+        return EmployeeAddress.builder()
+                .withAddress_line_1("Sample Address Line1")
+                .withAddress_line_2("Sample Address Line1")
+                .withState("Sample State")
+                .withPostal_code("Sample Postal Code")
+                .withCountry("Sample Country")
+                .build();
+    }
+}
---
+++ graphql-schema-first/src/main/java/com/krishna/banda/delegate/service/QueryService.java
@@ -0,0 +1,29 @@
+package com.krishna.banda.delegate.service;
+
+import com.krishna.banda.EmployeeData;
+import com.krishna.banda.util.DataFetchersDelegateQuery;
+import graphql.schema.DataFetchingEnvironment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class QueryService implements DataFetchersDelegateQuery {
+
+    @Override
+    /**
+     * @DataFetchingEnvironment contains all Metadata related to each Request
+     *
+     */
+    public EmployeeData getEmployee(DataFetchingEnvironment dataFetchingEnvironment, String employeeId) {
+        log.info("This is Entry Point");
+        log.info("Execution Id for the given request " + dataFetchingEnvironment.getExecutionId().toString());
+
+        return EmployeeData.builder()
+                .withEmployeeId(employeeId)
+                .withFirst_name("Sample First Name")
+                .withLast_name("Sample Last Name")
+                .build();
+    }
+}
